ebd996910b6825e24d5d6818c6d4bd99e681af3d
vpeil <vitali.peil@uni-bielefeld.de>
2014-09-25 08:10:32 +0200

    link corrected

--- lib/Catmandu/EuropePMC.pm
+++ lib/Catmandu/EuropePMC.pm
@@ -18,7 +18,7 @@ Catmandu::EuropePMC - A bundle of Catmandu modules for working with data from Eu
 
 <p>
 <a href="https://travis-ci.org/LibreCat/Catmandu-EuropePMC"><img src="https://travis-ci.org/LibreCat/Catmandu-EuropePMC.svg?branch=master" alt="build status" /></a>
-<a href="https://coveralls.io/repos/LibreCat/Catmandu-EuropePMC"><img src="https://coveralls.io/repos/LibreCat/Catmandu-EuropePMC/badge.png?branch=master" alt="coverage status" /></a>
+<a href="https://coveralls.io/r/LibreCat/Catmandu-EuropePMC"><img src="https://coveralls.io/repos/LibreCat/Catmandu-EuropePMC/badge.png?branch=master" alt="coverage status" /></a>
 <a href="http://cpants.cpanauthors.org/dist/Catmandu-EuropePMC"><img src="http://cpants.cpanauthors.org/dist/Catmandu-EuropePMC.png" alt="kwalitee score" /></a>
 </p>
 
