432f80cc28858c435c6db490f39a17aaf30325d9
Vitali Peil <vitali.peil@uni-bielefeld.de>
2018-02-14 12:05:04 +0100

    display status badges

--- lib/Catmandu/EuropePMC.pm
+++ lib/Catmandu/EuropePMC.pm
@@ -14,15 +14,13 @@ Catmandu::EuropePMC - A bundle of Catmandu modules for working with data from Eu
 
 =head1 STATUS
 
-=begin HTML
+=begin markdown
 
-<p>
-<a href="https://travis-ci.org/LibreCat/Catmandu-EuropePMC"><img src="https://travis-ci.org/LibreCat/Catmandu-EuropePMC.svg?branch=master" alt="build status" /></a>
-<a href="https://coveralls.io/r/LibreCat/Catmandu-EuropePMC"><img src="https://coveralls.io/repos/LibreCat/Catmandu-EuropePMC/badge.png?branch=master" alt="coverage status" /></a>
-<a href="http://cpants.cpanauthors.org/dist/Catmandu-EuropePMC"><img src="http://cpants.cpanauthors.org/dist/Catmandu-EuropePMC.png" alt="kwalitee score" /></a>
-</p>
+[![Build Status](https://travis-ci.org/LibreCat/Catmandu-EuropePMC.svg?branch=master)](https://travis-ci.org/LibreCat/Catmandu-EuropePMC)
+[![Test Coverage](https://coveralls.io/repos/LibreCat/Catmandu-EuropePMC/badge.png?branch=master)](https://coveralls.io/r/LibreCat/Catmandu-EuropePMC)
+[![CPANTS](http://cpants.cpanauthors.org/dist/Catmandu-EuropePMC.png)](http://cpants.cpanauthors.org/dist/Catmandu-EuropePMC)
 
-=end HTML
+=end markdown
 
 =head1 MODULES
 
